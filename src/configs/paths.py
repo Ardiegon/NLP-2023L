092794c76dbc592e861783872700a406d072cc62
Ardiegon <oskbsz@gmail.com>
2023-05-16 15:37:26 +0200

    encode with bert

---
+++ src/configs/paths.py
@@ -0,0 +1,5 @@
+from os.path import join
+
+DATA_DIR = join("..","data")
+DATA_NLP2023 = join(DATA_DIR, "nlp_2023_v2.csv")
+DATA_DUBLEQUALITY = join(DATA_DIR, "doubleQuality.csv")
---
+++ src/svm/process_data.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from enum import Enum
+from sentence_transformers import SentenceTransformer
+
+from configs.paths import DATA_DUBLEQUALITY, DATA_NLP2023
+
+class ModelNames(Enum):
+    POLISH = "Voicelab/sbert-base-cased-pl"
+    GERMAN = "T-Systems-onsite/cross-en-de-roberta-sentence-transformer"
+    MULTILINGUAL = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+
+def cosine(u, v):
+    return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+
+def test_models():
+    test_sentences = [
+        "Praca czyni człowiekiem sukcesu",
+        "Harte Arbeit und Ausdauer sind der Schlüssel zum Erfolg",
+        "Das Leben ist eine Reise, genieße jeden Moment.",
+        "Die Zeit heilt alle Wunden, sei geduldig.",
+        "Erfolg ist kein Glück, sondern harte Arbeit.",
+        "Życie to podróż, ciesz się każdą chwilą.",
+        "Czas leczy wszystkie rany, bądź cierpliwy.",
+        "Sukces to nie kwestia szczęścia, lecz ciężkiej pracy.",
+    ]
+
+    model = SentenceTransformer(ModelNames.MULTILINGUAL.value)
+    embeddings = model.encode(test_sentences)
+
+    for val, e in enumerate(embeddings[1:]):
+        sim = cosine(embeddings[0], e)
+        print("Sentence = ", test_sentences[val+1], "; similarity = ", sim)
+
+if __name__ == "__main__":
+    test_models()
+
