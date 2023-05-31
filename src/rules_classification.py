53c8918f3d4b1d267ffdcf630ad2e1abef7ec23d
arasheus <a.rasheuskaya@stud.elka.pw.edu.pl>
2023-06-01 00:47:00 +0200

    correct rules

--- src/rules_classification.py
+++ src/rules_classification.py
@@ -2,65 +2,41 @@ import pandas as pd
 import json
 import os
 import sys
-#from model_utils import plot_results
+from classification.model_utils import plot_results
 from data_management.load_data import load_dataset
 
 
-if len(sys.argv) < 4:
-    print("Too few arguments!")
+def predict(df, rules, column_name):
+    df['prediction'] = False
+    for rule in rules:
+        df.loc[:, 'prediction'] = [True if pred == True else True if rule in com else False for com,
+                                   pred in zip(df[column_name], df['prediction'])]
+    return df
+
+
+if len(sys.argv) < 3:
+    print("Too few arguments! You need to provide language and path to rules and, optionally, sentences to clasificate")
     sys.exit(1)
-else:
-    if sys.argv[3] == "train":
-        if len(sys.argv) < 7:
-            print("Too few arguments for training!")
-            sys.exit(1)
-
-        df = pd.read_csv(sys.argv[4], delimiter=sys.argv[7])
-        # print(df)
-        # print(df.columns)
-        content_column, label_column = [sys.argv[5], sys.argv[6]]
-        df = df[[content_column, label_column]]
-
-    elif sys.argv[3] == "test":
-        if len(sys.argv) == 4:
-            print("Nothing to test!")
-            sys.exit(1)
-        data = sys.argv
-    else:
-        print("Unknown mode!")
-        sys.exit(1)
 
 workdir = os.getcwd()
-df = load_dataset("polish")
-print(df)
 
-# with open(sys.argv[2]) as json_file:
-#     rules_settings = json.load(json_file)
+with open(sys.argv[2]) as json_file:
+    rules_settings = json.load(json_file)
 
-# rules = rules_settings["rules"][sys.argv[1]]
-# langs = rules_settings["rules"]
-# if sys.argv[1] not in langs:
-#     print("This language is not supported!")
-#     sys.exit(1)
+rules = rules_settings["rules"][sys.argv[1]]
+langs = rules_settings["rules"]
+if sys.argv[1] not in langs:
+    print("This language is not supported!")
+    sys.exit(1)
 
 
-# df[content_column] = df[content_column].apply(str.lower)
-# for ch in ['.', ',', '"', '"']:
-#     df[content_column] = df[content_column].str.replace(ch, '')
-# df['prediction'] = 0
-
-
-# for rule in rules:
-#     df.loc[:, 'prediction'] = [True if pred == True else True if rule in com else False for com,
-#                                pred in zip(df[content_column], df['prediction'])]
-
-
-# print(df['a'], df['prediction'])
-# # print(df[df[label_column] == df['prediction']].shape[0] / df.shape[0])
-
-
-# if sys.argv[3] == "train":
-#     #print(df[df[label_column] == df['prediction']].shape[0] / df.shape[0])
-#     plot_results(df['a'], df['prediction'])
-# else:
-#     pass
+if len(sys.argv) == 3:
+    df = predict(load_dataset(
+        rules_settings["translate_lang"][sys.argv[1]]), rules, 'comment')
+    plot_results(df['label'], df['prediction'])
+else:
+    sentences = sys.argv[3::]
+    df = predict(pd.DataFrame(data=sentences, columns=[
+                 'comment']), rules, 'comment')
+    for com, pred in zip(df['comment'], df['prediction']):
+        print(f"{com}: {pred}")
